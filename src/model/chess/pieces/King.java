011ac5c971aa10edc95777d26038eef4178e456d
LeonardoAndrad3 <leonardoxcontato@gmail.com>
2022-05-03 22:10:59 -0300

    Placing pieces on the board

---
+++ src/model/chess/pieces/King.java
@@ -0,0 +1,18 @@
+package model.chess.pieces;
+
+import model.boardgame.Board;
+import model.chess.ChessPiece;
+import model.chess.Color;
+
+public class King extends ChessPiece{
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString(){
+		return "k";
+	}
+
+}
